8234149a214aaa8af0e1378bfc0055e9026767ab
Thiago Terence <thiagorise@hotmail.com>
2024-08-27 19:16:43 -0300

    Add files via upload

---
+++ assets/main.js
@@ -0,0 +1,65 @@
+let textoDigitado = document.querySelector(".conteudo__texto")
+
+let campoVazio =  document.querySelector('.retorno__retorno-vazio')
+
+let campoCheio = document.querySelector('.retorno__retorno-cheio')
+
+let textoTransformado = ''
+
+function transformarTexto (funcao, texto) {
+    if (funcao === 'criptografar') {
+        textoTransformado = texto.replace(/e/gi, "enter").replace(/i/gi, "imes").replace(/a/gi, "ai").replace(/o/gi, "ober").replace(/u/gi, "ufat")
+    }else {
+         textoTransformado = texto.replace(/enter/gi, 'e').replace(/imes/gi, 'i').replace(/ai/gi, 'a').replace(/ober/gi, 'o').replace(/ufat/gi, 'u')
+    }
+    return textoTransformado;
+} 
+
+function exibeTexto(classe, texto) {
+    let campo = document.querySelector(classe);
+    campo.textContent = texto;
+}
+
+function ocultarElemento(elemento) {
+    elemento.style.display = 'none';
+}
+
+function exibirElemento(elemento) {
+    elemento.style.display = 'flex';
+}
+
+function limpaConteudo(classe) {
+    let campo = document.querySelector(classe);
+    campo.value = '';
+}
+
+function criptografar () {
+    if (textoDigitado.value === "") {
+        alert('Texto não encontrado \n\nDigite um Texto para ser criptografado')
+    }else {
+        ocultarElemento(campoVazio);
+        exibirElemento(campoCheio);
+        exibeTexto('.retorno__retorno-cheio__texto', transformarTexto('criptografar', textoDigitado.value));
+        limpaConteudo('.conteudo__texto');
+    }
+}       
+
+function descriptografar () {
+    if (textoDigitado.value === "") {
+        alert('Texto não encontrado \n\nDigite um Texto para ser criptografado');
+    }else {
+        ocultarElemento(campoVazio);
+        exibirElemento(campoCheio)
+        exibeTexto('.retorno__retorno-cheio__texto', transformarTexto('descriptografar', textoDigitado.value));
+        limpaConteudo('.conteudo__texto');
+    }
+}
+
+function copiarTexto() {
+    let texto = document.getElementById('texto-retorno').value;
+    navigator.clipboard.writeText(texto);
+    alert("Texto copiado!");
+
+    ocultarElemento(campoCheio)
+    exibirElemento(campoVazio)
+}
